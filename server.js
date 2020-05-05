1aaa7ab8010f6f56edce9dadcf8f49890487dc29
Zishen Ye <zishenye@MacBook-Pro.local>
2020-05-04 22:22:11 -0700

    fix issue that ajax call can return 304

--- server.js
+++ server.js
@@ -1,25 +1,25 @@
-console.log('server is staring');
+console.log('server is staring..');
 
 const express = require('express');
 
 let app = express();
 
-let server = app.listen(3000, () =>{
-    console.log('server is listening');
+let server = app.listen(3000, () => {
+    console.log('server is listening..');
 });
-
-//app.use(express.static('website'));
 
 app.use(express.static('website', {
     setHeaders: (res, path) => {
         res.setHeader('Cache-Control', 'no-cache');
+        // set http head to be cacheble but need to be validated before serving
     }
 }));
 
 
 app.get('/homePage', function (req, res) {
     setTimeout( () => {
+        res.set('Cache-Control', 'no-store');
+        // set dynamic content to be non cacheble to either CDN or browser
         res.sendFile(__dirname + '/views/homePage.html');
     }, 3000);
-    //res.sendFile(__dirname + '/views/homePage.html');
 });
--- website/javascript/bodyInjector.js
+++ website/javascript/bodyInjector.js
@@ -4,7 +4,7 @@
          document.getElementById('oldBody').innerHTML = '';
          var xhttp = new XMLHttpRequest();
 
-         xhttp.open("GET", "http://localhost:3000/homePage", false);
+         xhttp.open("GET", "/homePage", false);
          xhttp.send();
          document.getElementById('oldBody').innerHTML = xhttp.responseText;
 
